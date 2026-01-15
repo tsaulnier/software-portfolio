21f531aa442932e608d8c15e9cc97810b158b2f3
Thom Saulnier <75642487+tsaulnier@users.noreply.github.com>
2026-01-15 14:21:22 -0500

    fixed a small problem with an unused font causing a crash

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en"
+    suppressHydrationWarning
+    className={ibmPlexMono.variable}>
       <head>
         <script
           dangerouslySetInnerHTML={{
